a6539d9b62c100f21de1fecc3afe6912b406fe20
alzberalalzber-boop <alzberalalzber@gmail.com>
2026-04-24 17:03:38 +0300

    Add TextInput import to main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import threading
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from android.permissions import request_permissions, Permission
